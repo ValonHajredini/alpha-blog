112ec3842117f40ed0a65ff44fc6b6e1babcf5db
Valon Hajredini <hajredini.valon@gmail.com>
2016-06-11 14:56:27 +0200

    fix create add errors display and notes on layout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,13 +3,19 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  def show
+    @article = Article.find(params[:id])
+  end
 
   def create
-    # render plain: params[:article].inspect
     @article = Article.new(article_params)
     if @article.save
-      redirect_to articles_show(@article)
-    end
+      flash[:notice] = "Article wos successfully created"
+      redirect_to article_path(@article)
+
+  else
+    render 'new'
+  end
   end
   private
   def article_params
