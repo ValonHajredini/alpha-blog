c475300a86fc2dbfb4f0a99da9f60d677e2a6be3
Valon Hajredini <hajredini.valon@gmail.com>
2016-06-14 01:36:19 +0200

    Stailing And add user dfata

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ end
   def create
 
     @article = Article.new(article_params)
-    # @article.user = User.first
+     @article.user = User.first
 
     if @article.save
       flash[:success] = "Article wos successfully created"
