f325d54d916f90207d2509eacb2bb148b7c668d0
Valon Hajredini <hajredini.valon@gmail.com>
2016-06-15 00:33:20 +0200

    Editing the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,18 @@ class UsersController < ApplicationController
     end
 
   end
+  def edit
+  @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your acount wos updated successfully"
+      redirect_to edit_user_path(@user)
+    else
+      render "new"
+    end
+  end
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
