5a94148e3b65c50ac923807b3234c3cd0321168b
Valon Hajredini <hajredini.valon@gmail.com>
2016-06-15 02:47:29 +0200

    Shtimi i sessjonit dhe fshirja e tij

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,21 @@ class SessionsController < ApplicationController
 
   end
   def create
-    @email = params[:session][:email]
+    email = params[:session][:email]
+    password = params[:session][:password]
+    user = User.find_by(email: email.downcase)
+    if user && user.authenticate(password)
+      flash[:success]= "You have successfully loged in"
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      flash.now[:danger] = "Wrong email or password"
+      render 'new'
+    end
   end
   def destroy
-
+    session[:user_id] = nil
+    flash[:success] = "You have loged out"
+    redirect_to root_path
   end
 end
