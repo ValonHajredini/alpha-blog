355e707463ab2048703c0897b3402505bb9f7b97
Valon Hajredini <hajredini.valon@gmail.com>
2016-07-07 09:48:27 +0200

    Admin validated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user ,only: [:edit, :update, :show]
+  before_action :set_user ,only: [ :update, :show]
   before_action :require_same_user, only: [:edit, :update]
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
 
   end
   def edit
+    @user = User.find(params[:id])
   end
   def update
     if @user.update(user_params)
